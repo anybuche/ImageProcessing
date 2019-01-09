3593b471313f185b588709cd616ac156578ee5c2
Unknown <anybuche@hotmail.com>
2019-01-09 17:04:02 +0100

    Final ?

--- project.m
+++ project.m
@@ -14,71 +14,45 @@ close all;
 gh2013 = images.landsat2013(:,:,5)-images.landsat2013(:,:,7);
 gh2018 = images.landsat2018(:,:,5)-images.landsat2018(:,:,7);
 
-% Subtract the two bands with the most difference (Sentinel)
-gh2015s = images.sentinel2015(:,:,7)-images.sentinel2015(:,:,12);
-gh2018s = images.sentinel2018(:,:,7)-images.sentinel2018(:,:,12);
-
-% Normalized indexes
-gh2018n = (images.landsat2018(:,:,5)-images.landsat2018(:,:,7))./(images.landsat2018(:,:,5)+images.landsat2018(:,:,7));
-gh2013n = (images.landsat2013(:,:,5)-images.landsat2013(:,:,7))./(images.landsat2013(:,:,5)+images.landsat2013(:,:,7));
-
 % Bring out the difference for Landsat images
 figure('name', 'Greenhouses 2013')
 imshow(gh2013)
 figure('name', 'Greenhouses 2018')
 imshow(gh2018)
-% Histogram matching for better accuracy
+
+%% Histogram matching for better accuracy
+
 gh2013m = imhistmatch(gh2013,gh2018);
 landsatDiff = gh2018-gh2013m;
-figure('name', 'Greenhouses difference')
+figure('name', 'Greenhouses difference 2013-2018')
 imshow(landsatDiff);
 
-% Bring out the difference for Sentinel images
-figure('name', 'Greenhouses 2013s')
-imshow(gh2015s)
-figure('name', 'Greenhouses 2018s')
-imshow(gh2018s)
-% Histogram matching for better accuracy
-gh2015ms = imhistmatch(gh2015s,gh2018s);
-sentinelDiff = gh2018s-gh2015ms;
-figure('name', 'Greenhouses difference')
-imshow(sentinelDiff);
+%% Thresholding
 
-figure('name', 'Greenhouses 2018 norm')
-imshow(gh2018n);
-figure('name', 'Greenhouses 2013 norm')
-imshow(gh2013n);
-% Histogram matching for better accuracy
-gh2013nm = imhistmatch(gh2013n,gh2018n);
-figure('name', 'Greenhouses difference norm')
-imshow(gh2018n-gh2013nm);
-
-%Thresholding (Landsat)
 landsatDiff_uint8 = uint8(landsatDiff*255);
-landsatDiffth = landsatDiff_uint8>70; %This Threshold is to be adjusted
-figure('name', 'Greenhouses difference thresholded (Landsat)')
+landsatDiffth = landsatDiff_uint8 > 70; %This Threshold is to be adjusted
+figure('name', 'Greenhouses difference thresholded')
 imshow(landsatDiffth)
 
-%Thresholding (Sentinel)
-sentinelDiff_uint8 = uint8(sentinelDiff*255);
-sentinelDiffth = sentinelDiff_uint8>70; %This Threshold is to be adjusted
-figure('name', 'Greenhouses difference thresholded (Sentinel)')
-imshow(sentinelDiffth)
+%% Removing small artifacts by applying an opening filter
 
-% Removing small artifacts by applying an opening filter
-SE = strel('diamond',2); % 'diamond' 'square' %This size also has to be adjusted
+SE = strel('diamond',2); %This size and shape also can be adjusted
 % Performing Opening
 landsatDiff_op = imopen(landsatDiffth,SE);
-sentinelDiff_op = imopen(sentinelDiffth,SE);
-figure('name', 'Greenhouses difference thresholded and openend (Landsat)')
+figure('name', 'Greenhouses difference thresholded and openend')
 imshow(landsatDiff_op)
-figure('name', 'Greenhouses difference thresholded and openend (Sentinel)')
-imshow(sentinelDiff_op)
 
+%% Estimate the number of new greenhouses
 
-%% Kmeans algorithm
-k = 6; %2 clusters
-n_iter = 100;
+n = sum(sum(landsatDiff_op));
+A = n*30*30/(100*100); %
+A_tot = size(landsatDiff,1)*size(landsatDiff,2)*30*30/(100*100);
+fprintf('Nombre de nouvelles hectares de serres: %d\n over %d total hectares\n', A, A_tot)
+
+%% Kmeans algorithm on the difference between years
+
+k = 3; %2 clusters
+n_iter = 20;
 
 landsatDiff_reshape = reshape(landsatDiff,size(landsatDiff,1)*size(landsatDiff,2),size(landsatDiff,3));
 
@@ -89,9 +63,10 @@ imagesc(reshape(kmeansDiff,size(landsatDiff,1),size(landsatDiff,2)));
 title('k_means Diff');
 axis equal tight
 
-%%
-k = 4; %4 clusters
-n_iter = 30;
+%% Kmeans algorithm on the 2013 full image
+
+k = 5; %4 clusters
+n_iter = 20;
 
 im2013_reshape = reshape(images.landsat2013,size(images.landsat2013,1)*size(images.landsat2013,2),size(images.landsat2013,3));
 kmeans2013 = k_means(im2013_reshape,k,n_iter);
@@ -103,9 +78,10 @@ xlabel('x')
 ylabel('y')
 
 
-%% Kmeans algorithm
-k = 4; %4 clusters
-n_iter = 4;
+%% Kmeans algorithm on the difference between bands in 2013 and 2018
+
+k = 3; %4 clusters
+n_iter = 20;
 
 % for 2013 and 2018 images
 im2013_reshape = reshape(gh2013,size(gh2013,1)*size(gh2013,2),size(gh2013,3));
@@ -128,33 +104,3 @@ title('k_means 2018');
 axis equal tight
 xlabel('x')
 ylabel('y')
-
-%for nomalized images
-im2013n_reshape = reshape(gh2013n,size(gh2013n,1)*size(gh2013n,2),size(gh2013n,3));
-im2018n_reshape = reshape(gh2018n,size(gh2018n,1)*size(gh2018n,2),size(gh2018n,3));
-
-kmeans2013 = k_means(im2013n_reshape,k,n_iter);
-kmeans2018 = k_means(im2018n_reshape,k,n_iter);
-
-figure('name', 'kmeans on normalized indexes')
-subplot(121);
-imagesc(reshape(kmeans2013,size(gh2013n,1),size(gh2013n,2)));
-title('k_means 2013 normalized');
-axis equal tight
-xlabel('x')
-ylabel('y')
-
-subplot(122);
-imagesc(reshape(kmeans2018,size(gh2018n,1),size(gh2018n,2)));
-title('k_means 2018 normalized');
-axis equal tight
-xlabel('x')
-ylabel('y')
-
-% WORKS MUCH BETTER ON NORMALIZED PICTURES
-
-
-
-
-
-
